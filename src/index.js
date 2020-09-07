ebf6abf8623b1708f42d433a2e2181aa468c79bb
sungyu kim <liddle99@naver.com>
2020-09-07 15:37:21 +0900

    practice_redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// createStore 와 루트 리듀서 불러오기
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+// **** (1) Provider 불러오기
+import { Provider } from 'react-redux';
 
+
+import App from './App';
+
+// 리덕스 개발자도구 적용
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+// **** (2) Provider 렌더링해서 기존의 App 감싸주기
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
