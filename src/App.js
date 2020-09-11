e4166b433ecc717daff23dcc38b8d427847396cd
sungyu kim <liddle9985@gmail.com>
2020-09-12 03:49:11 +0900

    test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import CounterContainer from "./container/CounterContainer";
 import PaletteContainer from "./container/PaletteContainer";
 import WaitingListContainer from "./container/WaitingListContainer";
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PaletteContainer selected="red" />
+        <PaletteContainer selected="blue" />
         <CounterContainer />
         <WaitingListContainer />
       </div>
